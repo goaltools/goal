908e0a63380937e03d5bf4e2668b04fb8a8261e9
Alexander Kucherenko <alkchr@colegion.com>
2015-03-24 20:48:08 +0600

    Finished NewType method and its tests

--- command/command.go
+++ command/command.go
@@ -1,22 +1,37 @@
 // Package command is used for parsing input parameters.
 package command
 
-import (
-	"github.com/anonx/ok/log"
-)
+import ()
+
+// IncorrectArgsErr is returned every time a user is trying
+// to use input parameters we do not expect.
+var IncorrectArgsErr error
 
 // Type is a main type of command package.
 // It is used for storage of parsed parameters.
 type Type struct {
 	action string
+	params map[string]string
 }
 
-// NewType initializes and returns Type object.
-// It expects even number of args. Otherwise, it will panic.
-func NewType(args []string) Type {
-	// Make sure the number of arguments is even number.
-	if len(args)%2 != 0 {
-		log.Error.Panic("Incorrect number of parameters.")
+// NewType initializes and returns Type object. It expects even number of args.
+// Otherwise, it an IncorIncorrectArgsErr error will be returned.
+func NewType(args []string) (*Type, error) {
+	// Make sure the number of arguments is even number
+	// and it is more than zero.
+	if len(args) == 0 || len(args)%2 != 0 {
+		return nil, IncorrectArgsErr
 	}
-	return Type{}
+
+	// Save the arguments as dict.
+	params := map[string]string{}
+	for i := 0; i < len(args); i += 2 {
+		params[args[i]] = args[i+1]
+	}
+
+	// The first argument is a requested action.
+	return &Type{
+		action: args[0],
+		params: params,
+	}, nil
 }
