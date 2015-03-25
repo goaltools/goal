c24aef22311d542c14dc603edb7b76ab5839b874
Alexander Kucherenko <alkchr@colegion.com>
2015-03-25 13:56:44 +0600

    Help command has been added

---
+++ help/help.go
@@ -0,0 +1,25 @@
+// Package help is a command that shows info
+// about the commands 'ok' toolkit supports.
+package help
+
+import (
+	"github.com/anonx/ok/log"
+)
+
+// Start is an entry point of help command.
+func Start(params map[string]string) {
+	switch params["help"] {
+	default:
+		log.Info.Println(info)
+	}
+}
+
+var info = `ok command argument [arguments]
+
+The commands are:
+	new         create a skeleton application
+	run         run a watcher / task runner
+	generate    analize files, build handlers, routes, etc.
+
+Use "ok help command" for more information.
+`
