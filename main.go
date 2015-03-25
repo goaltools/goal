22b1fb2e2ac57b763d673df0c3656d58073b94ca
alconaut <alconautgh@yandex.com>
2015-03-25 13:56:44 +0600

    Help command has been added

--- main.go
+++ main.go
@@ -1,6 +1,30 @@
 package main
 
-import ()
+import (
+	"os"
+
+	"github.com/anonx/ok/command"
+	"github.com/anonx/ok/help"
+	"github.com/anonx/ok/log"
+)
+
+// Handlers is a list of registered commands
+// 'ok' toolkit supports.
+var Handlers = map[string]command.Handler{
+	"help": help.Start,
+}
 
 func main() {
+	// Validate input parameters and find out what user wants to run.
+	ct, err := command.NewType(os.Args[1:])
+	if err != nil {
+		// Validation failed because of incorrect arguments number,
+		// try to show help menu instead.
+		ct, err = command.NewType([]string{"help", "info"})
+	}
+	err = ct.Register(Handlers)
+	if err != nil {
+		// Validation failed because requested handler does not exist.
+		log.Warn.Printf("Unknown command '%s'.\nRun 'ok help' for usage.", os.Args[1])
+	}
 }
