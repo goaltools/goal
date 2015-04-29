8febfa7ed8cd1bde31311577860972477ab0c347
Alexander Kucherenko <alkchr@colegion.com>
2015-04-29 16:56:45 +0600

    Added example application

---
+++ example/controllers/init.go
@@ -0,0 +1,6 @@
+package controllers
+
+// Controller is a struct that should be embedded into every controller
+// to make such methods as Render, RenderJSON, etc. available.
+type Controller struct {
+}
