f72b207cddd20881d756caebf36d92ec70c9a784
alconaut <alconautgh@yandex.com>
2015-05-05 11:06:57 +0600

    Added scan command

--- scan/scan.go
+++ scan/scan.go
@@ -2,3 +2,17 @@
 // requested directory and generates a .go file with
 // a map of all found files.
 package scan
+
+import (
+	"github.com/anonx/ok/log"
+)
+
+// Start is an entry point of scan command.
+func Start(action string, params map[string]string) {
+	if params[action] != "directory" {
+		log.Warn.Printf(
+			"I do not know how to scan '%s'.\nRun 'ok help %s' for more information.",
+			params[action], action,
+		)
+	}
+}
