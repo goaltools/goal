6e36ce91972062f48a0e456f3bf98549be926918
Alexander Kucherenko <alkchr@colegion.com>
2015-05-12 16:51:28 +0600

    Make possible the initialization of Paths in template middleware

--- middleware/template/html.go
+++ middleware/template/html.go
@@ -2,21 +2,13 @@ package template
 
 import (
 	"net/http"
-
-	"github.com/anonx/sunplate/action"
 )
 
 // HTML is a result that is returned from actions by default.
 // This struct implements action.Result interface.
 type HTML struct {
-	basePath string
 }
 
 // Apply writes to response the result received from action.
 func (t *HTML) Apply(w http.ResponseWriter, r *http.Request) {
-}
-
-// RenderTemplate initializes and returns HTML type that implements Result interface.
-func (t *Middleware) RenderTemplate(templatePath string) action.Result {
-	return &HTML{}
 }
