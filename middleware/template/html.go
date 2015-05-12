72edcd120f4185297a3dc4dc26815711812f0bda
alconaut <alconautgh@yandex.com>
2015-05-12 13:09:10 +0600

    Remove BasePath method

--- middleware/template/html.go
+++ middleware/template/html.go
@@ -12,11 +12,6 @@ type HTML struct {
 	basePath string
 }
 
-// BasePath initializes structure with the default value of app's path.
-func (t *HTML) BasePath(s string) {
-	t.basePath = s
-}
-
 // Apply writes to response the result received from action.
 func (t *HTML) Apply(w http.ResponseWriter, r *http.Request) {
 }
