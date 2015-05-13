9d59d04e9a2a5a862b7325853950845a6fad4e75
Alexander Kucherenko <alkchr@colegion.com>
2015-05-13 19:01:49 +0600

    Added Finish method to result

--- action/result.go
+++ action/result.go
@@ -11,4 +11,9 @@ import (
 type Result interface {
 	// Apply writes the structure content to the response writer.
 	Apply(w http.ResponseWriter, r *http.Request)
+
+	// Finish is a method that returns true if it is expected
+	// server should return result to the client.
+	// No other results will be applied after a result that is finished.
+	Finish() bool
 }
