c415500abcc5794b935c6be86bb856dc88bd45af
Alexander Kucherenko <alkchr@colegion.com>
2015-05-14 14:33:15 +0600

    Added Delims and Funcs variables to template

--- middleware/template/template.go
+++ middleware/template/template.go
@@ -4,26 +4,41 @@
 package template
 
 import (
+	"html/template"
+
 	"github.com/anonx/sunplate/action"
 )
 
-// Paths is a map of templates.
-// Template names are represented as keys and their paths as values.
-// Initialize it from your init.go as follows:
-//	import (
-//		"github.com/anonx/sunplate/assets/views"
-//		"github.com/anonx/sunplate/middleware/template"
-//	)
-//
-//	type Controller struct {
-//		// ...
-//		template.Middleware
-//	}
-//
-//	func init() {
-//		template.Paths = views.Context
-//	}
-var Paths = map[string]string{}
+var (
+	// Paths is a map of templates.
+	// Template names are represented as keys and their paths as values.
+	// Initialize it from your init.go as follows:
+	//	import (
+	//		"github.com/anonx/sunplate/assets/views"
+	//		"github.com/anonx/sunplate/middleware/template"
+	//	)
+	//
+	//	type Controller struct {
+	//		// ...
+	//		template.Middleware
+	//	}
+	//
+	//	func init() {
+	//		template.Paths = views.Context
+	//	}
+	Paths = map[string]string{}
+
+	// Delims are action delimiters that are used for call to Parse.
+	// Empty delimiters activate default: {{ and }}.
+	Delims struct {
+		Left, Right string
+	}
+
+	// Funcs are added to the template's function map.
+	// Functions are expected to return just 1 argument or
+	// 2 in case the second one is of error type.
+	Funcs template.FuncMap
+)
 
 // Middleware is a main type that should be embeded into controller structs.
 type Middleware struct {
