cf01d715a6a205645d0badc5bb71c3f6de99e9c2
Alexander Kucherenko <alkchr@colegion.com>
2015-06-17 22:36:43 +0600

    Starting a work on handlers generation

--- generation/handlers/handlers.go
+++ generation/handlers/handlers.go
@@ -7,7 +7,24 @@ import (
 
 	"github.com/anonx/sunplate/command"
 	"github.com/anonx/sunplate/generation/output"
+	"github.com/anonx/sunplate/reflect"
 )
+
+const (
+	// ActionInterfaceImport is a GOPATH to the Result interface
+	// that should be implemented by types returned by actions.
+	ActionInterfaceImport = "github.com/anonx/sunplate/action"
+
+	// ActionInterfaceName is an interface that should be implemented
+	// by types that are returned from actions.
+	ActionInterfaceName = "Result"
+)
+
+// Controller is a type that represents application controller,
+// a structure that has actions.
+type Controller struct {
+	Actions reflect.Funcs // Actions are methods that implement action.Result interface.
+}
 
 // Start is an entry point of the generate handlers command.
 func Start(basePath string, params command.Data) {
@@ -21,4 +38,10 @@ func Start(basePath string, params command.Data) {
 		"rootPath": params.Default("--path", "./controllers/"),
 	}
 	t.Generate()
+}
+
+// extractControllers gets a reflect.Package type and returns
+// a slice of controllers that are found there.
+func extractControllers(pkg *reflect.Package) []Controller {
+	return nil
 }
