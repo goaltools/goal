7d8433508005e656aba79794b4a8a415063992a2
Alexander Kucherenko <alkchr@colegion.com>
2015-07-02 14:18:30 +0600

    Better test coverage of output

--- generation/output/helpers_test.go
+++ generation/output/helpers_test.go
@@ -1,17 +1,1 @@
 package output
-
-import (
-	"testing"
-)
-
-func TestExport(t *testing.T) {
-	if r := export(""); r != "" {
-		t.Errorf(`Empty input, the same string was expected as a result. Got "%s".`, r)
-	}
-	if r := export("a"); r != "A" {
-		t.Errorf(`Incorrect result. Expected "A", got "%s".`, r)
-	}
-	if r := export("int"); r != "Int" {
-		t.Errorf(`Incorrect result. Expected "Int", got "%s".`, r)
-	}
-}
