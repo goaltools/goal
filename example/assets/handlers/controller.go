144311ca506e478282bb63a1fd200cb645b603ec
alconaut <alconautgh@yandex.com>
2015-07-04 20:12:34 +0600

    Updated example app

--- example/assets/handlers/controller.go
+++ example/assets/handlers/controller.go
@@ -8,6 +8,7 @@ import (
 	c0 "github.com/anonx/sunplate/example/assets/handlers/github.com/anonx/sunplate/middleware/template"
 	contr "github.com/anonx/sunplate/example/controllers"
 
+	a "github.com/anonx/sunplate/action"
 	"github.com/anonx/sunplate/strconv"
 )
 
@@ -20,22 +21,38 @@ type Controller struct {
 }
 
 // New allocates (github.com/anonx/sunplate/example/controllers).Controller controller,
-// initializes its parents; then returns.
+// initializes its parents; then returns the controller.
 func (t Controller) New() *contr.Controller {
 	c := &contr.Controller{}
 	c.Middleware = c0.Middleware{}.New()
 	return c
 }
 
-// Before executes magic Before actions of parent controllers
-// and calls Before action of (github.com/anonx/sunplate/example/controllers).Controller with arguments
-// that are extracted from r.Form and converted to appropriate type.
-func (t Controller) Before(c *contr.Controller, w http.ResponseWriter, r *http.Request) {
-	// Execute magic Before actions of parent controllers.
-	c0.Middleware{}.Before(c.Middleware, w, r)
-
+// Before executes magic actions of embedded controllers, and
+// calls (github.com/anonx/sunplate/example/controllers).Controller.Before with arguments
+// that are extracted from r.Form and converted to appropriate types.
+func (t Controller) Before(c *contr.Controller, w http.ResponseWriter, r *http.Request) a.Result {
+	// Execute magic Before actions of embedded controllers.
+	if r := (c0.Middleware{}.After(c.Middleware, w, r)); r != nil {
+		return r
+	}
 	// Call magic Before action of (github.com/anonx/sunplate/example/controllers).Controller.
-	c.Before()
+	if r := c.Before( // Parameters should be binded.
+	); r != nil {
+		return r
+	}
+	// Continue execution chain.
+	return nil
+}
+
+// After executes magic actions of embedded controllers.
+func (t Controller) After(c *contr.Controller, w http.ResponseWriter, r *http.Request) a.Result {
+	// Execute magic After actions of embedded controllers.
+	if r := (c0.Middleware{}.After(c.Middleware, w, r)); r != nil {
+		return r
+	}
+	// Continue execution chain.
+	return nil
 }
 
 func init() {
