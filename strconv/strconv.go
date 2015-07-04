407bf64e3cf389d5a452ac024fc2db93d854e61c
Alexander Kucherenko <alkchr@colegion.com>
2015-07-04 11:44:30 +0600

    Added Render function to strconv

--- strconv/strconv.go
+++ strconv/strconv.go
@@ -4,6 +4,8 @@
 package strconv
 
 import (
+	"errors"
+	"fmt"
 	"go/ast"
 	"net/url"
 	"strconv"
@@ -238,6 +240,56 @@ func Float64s(vs url.Values, k string, is ...int) (as []float64) {
 	Below are various helper functions.
 */
 
+// get receives url.Values, a key, and an index (as a slice, but only
+// the first element is used). Then returns vs[k][is[0]].
+// If the url.Values or slice of indexes is empty or key is not found
+// empty string will be returned.
+func get(vs url.Values, k string, is []int) string {
+	// Get index from the first element of indexes slice
+	// or use the default 0.
+	i := 0
+	if len(is) > 0 {
+		i = is[0]
+	}
+
+	// Make sure such key does exist and index is in range.
+	// Return "" otherwise.
+	ss := vs[k]
+	if len(ss) <= i {
+		return ""
+	}
+
+	// Return the final result.
+	return vs[k][i]
+}
+
+/*
+	Below are code generation related functions, methods, and types.
+*/
+
+// UnsupportedTypeErr is an error that indicates that there is no conversion
+// function for the requested type.
+var ErrUnsupportedType = errors.New("unsupported type")
+
+// FnMap is a mapping between type names and appropriate conversion functions.
+type FnMap map[string]reflect.Func
+
+// Render gets a package name, name of url.Values variable and an argument, and renders
+// an appropriate strconv Function, e.g. `strconv.Int(r.Form, "number")`.
+// If the argument is not supported error will be returned as a second argument.
+// This is used for code generation.
+func (m FnMap) Render(pkgName, vsName string, a reflect.Arg) (string, error) {
+	// Make sure argument is of supported type.
+	t := a.Type.String()
+	f, ok := m[t]
+	if !ok {
+		return "", ErrUnsupportedType
+	}
+
+	// Return the fragment of code we need.
+	return fmt.Sprintf(`%s.%s(%s, "%s")`, pkgName, f.Name, vsName, a.Name), nil
+}
+
 // Context returns mappings between types that can be parsed using
 // this package and functions for that conversions.
 // All conversion functions meet the following criterias:
@@ -245,8 +297,8 @@ func Float64s(vs url.Values, k string, is ...int) (as []float64) {
 // 2. They expect 3 arguments: url.Values, string, ...int.
 // 3. They return 1 argument.
 // This is useful for code generation.
-func Context() map[string]reflect.Func {
-	fs := map[string]reflect.Func{}
+func Context() FnMap {
+	fs := FnMap{}
 	p := reflect.ParseDir(path.SunplateDir("strconv"))
 	for i := range p.Funcs {
 		if !strconvFunc(p.Funcs[i]) {
@@ -281,27 +333,4 @@ func strconvFunc(f reflect.Func) (r bool) {
 		return
 	}
 	return true
-}
-
-// get receives url.Values, a key, and an index (as a slice, but only
-// the first element is used). Then returns vs[k][is[0]].
-// If the url.Values or slice of indexes is empty or key is not found
-// empty string will be returned.
-func get(vs url.Values, k string, is []int) string {
-	// Get index from the first element of indexes slice
-	// or use the default 0.
-	i := 0
-	if len(is) > 0 {
-		i = is[0]
-	}
-
-	// Make sure such key does exist and index is in range.
-	// Return "" otherwise.
-	ss := vs[k]
-	if len(ss) <= i {
-		return ""
-	}
-
-	// Return the final result.
-	return vs[k][i]
 }
