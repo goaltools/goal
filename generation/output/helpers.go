e060aa5c3f6406ac8d0bc3b097abe7b19f4f310b
alconaut <alconautgh@yandex.com>
2015-07-04 20:12:11 +0600

    Implemented generation of before & after magic actions

--- generation/output/helpers.go
+++ generation/output/helpers.go
@@ -1,10 +1,34 @@
 package output
 
 import (
+	"fmt"
 	"path/filepath"
 	"text/template"
 )
 
 var funcs = template.FuncMap{
-	"join": filepath.Join,
+	"dict":    dict,
+	"join":    filepath.Join,
+	"set":     set,
+	"sprintf": fmt.Sprintf,
+}
+
+// dict joins a bunch of maps into one and returns it.
+func dict(as ...map[string]interface{}) map[string]interface{} {
+	m := map[string]interface{}{}
+	for i := 0; i < len(as); i++ {
+		for k := range as[i] {
+			m[k] = as[i][k]
+		}
+	}
+	return m
+}
+
+// set gets a key string and a value interface{}
+// and returns them as a map.
+func set(k string, v interface{}) map[string]interface{} {
+	m := map[string]interface{}{
+		k: v,
+	}
+	return m
 }
