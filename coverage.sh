92bd299bed2c6557fd7e090660462c62a64a21b5
alconaut <alconautgh@yandex.com>
2015-07-19 17:17:08 +0600

    Do not ignore internal directory

--- coverage.sh
+++ coverage.sh
@@ -12,7 +12,7 @@ echo "mode: set" > acc.out
 fail=0
 
 # Standard go tooling behavior is to ignore dirs with leading underscors
-for dir in $(find . -maxdepth 10 -not -path "*testdata*" -not -path './.git*' -not -path '*/_*' -not -path '*internal*' -not -path './example*' -not -path "./middleware" -type d);
+for dir in $(find . -maxdepth 10 -not -path "*testdata*" -not -path './.git*' -not -path '*/_*' -type d);
 do
 	if ls $dir/*.go &> /dev/null; then
 		go test -coverprofile=profile.out $dir || fail=1
