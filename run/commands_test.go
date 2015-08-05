c8880bca218b70f1af331ecdd0c5a4ea158c684c
alconaut <alconautgh@yandex.com>
2015-08-05 17:43:34 +0600

    100% test coverage for run subpackage

---
+++ run/commands_test.go
@@ -0,0 +1,56 @@
+package run
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStart_IncorrectTask(t *testing.T) {
+	ts := []string{
+		"gxgbhsjdjdduuhdhsh", // This is expected to cause a panic.
+	}
+	defer expectPanic(fmt.Sprintf(`Command "%s" does not exist. Panic expected.`, ts[0]))
+	start(ts)
+}
+
+func TestStart(t *testing.T) {
+	ts := []string{
+		`sunplate start_smth`,
+		`sunplate start_smth_else`,
+	}
+	start(ts)
+}
+
+func TestRun_IncorrectTask(t *testing.T) {
+	ts := []string{
+		"gxgbhsjdjdduuhdhsh", // This is expected to cause a panic.
+	}
+	defer expectPanic(fmt.Sprintf(`Command "%s" does not exist. Panic expected.`, ts[0]))
+	run(ts)
+}
+
+func TestRun(t *testing.T) {
+	ts := []string{
+		`sunplate run_smth`,
+		`sunplate run_smth_else`,
+	}
+	run(ts)
+}
+
+func TestStartSingleInstance_IncorrectCommand(t *testing.T) {
+	go instanceController()
+
+	startSingleInstance("smth", "gxgbhsjdjdduuhdhsh")
+	<-stopped
+}
+
+func TestStartSingleInstance(t *testing.T) {
+	go instanceController()
+
+	startSingleInstance("app", "sunplate run github.com/anonx/sunplate/skeleton")
+	startSingleInstance("smth", "sunplate help")
+	startSingleInstance("app", "sunplate run github.com/anonx/sunplate/skeleton")
+
+	channel <- message{action: "exit"}
+	<-stopped
+}
