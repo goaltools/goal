e9f43b68062bcde7859da1eee3152b4f3c3375c6
Alexander Kucherenko <alkchr@colegion.com>
2015-08-06 13:59:18 +0600

    Move code generation stuff of strconv to internals

--- strconv/strconv.go
+++ strconv/strconv.go
@@ -4,15 +4,9 @@
 package strconv
 
 import (
-	"errors"
-	"fmt"
-	"go/ast"
 	"net/url"
 	"strconv"
 	"strings"
-
-	"github.com/anonx/sunplate/internal/path"
-	"github.com/anonx/sunplate/internal/reflect"
 )
 
 /*
@@ -261,78 +255,6 @@ func get(vs url.Values, k string, is []int) string {
 
 	// Return the final result.
 	return vs[k][i]
-}
-
-/*
-	Below are code generation related functions, methods, and types.
-*/
-
-// UnsupportedTypeErr is an error that indicates that there is no conversion
-// function for the requested type.
-var ErrUnsupportedType = errors.New("unsupported type")
-
-// FnMap is a mapping between type names and appropriate conversion functions.
-type FnMap map[string]reflect.Func
-
-// Render gets a package name, name of url.Values variable and an argument, and renders
-// an appropriate strconv Function, e.g. `strconv.Int(r.Form, "number")`.
-// If the argument is not supported error will be returned as a second argument.
-// This is used for code generation.
-func (m FnMap) Render(pkgName, vsName string, a reflect.Arg) (string, error) {
-	// Make sure argument is of supported type.
-	t := a.Type.String()
-	f, ok := m[t]
-	if !ok {
-		return "", ErrUnsupportedType
-	}
-
-	// Return the fragment of code we need.
-	return fmt.Sprintf(`%s.%s(%s, "%s")`, pkgName, f.Name, vsName, a.Name), nil
-}
-
-// Context returns mappings between types that can be parsed using
-// this package and functions for that conversions.
-// All conversion functions meet the following criterias:
-// 1. They are exported.
-// 2. They expect 3 arguments: url.Values, string, ...int.
-// 3. They return 1 argument.
-// This is useful for code generation.
-func Context() FnMap {
-	fs := FnMap{}
-	p := reflect.ParseDir(path.SunplateDir("strconv"))
-	for i := range p.Funcs {
-		if !strconvFunc(p.Funcs[i]) {
-			continue
-		}
-		fs[p.Funcs[i].Results[0].Type.String()] = p.Funcs[i]
-	}
-	return fs
-}
-
-// strconvFunc gets a reflect.Func and detects whether it is
-// a string conversion function.
-func strconvFunc(f reflect.Func) (r bool) {
-	// Make sure the function is exported.
-	if !ast.IsExported(f.Name) {
-		return
-	}
-
-	// There are should be 3 arguments: url.Values, string, ...int.
-	if len(f.Params) < 3 {
-		return
-	}
-	ps := []string{"url.Values", "string", "...int"}
-	for i := range ps {
-		if f.Params[i].Type.String() != ps[i] {
-			return
-		}
-	}
-
-	// It should return 1 parameter.
-	if len(f.Results) != 1 {
-		return
-	}
-	return true
 }
 
 // MeaningOfLife is the answer to the Ultimate Question of Life,
--- strconv/strconv_test.go
+++ strconv/strconv_test.go
@@ -4,8 +4,6 @@ import (
 	"net/url"
 	"reflect"
 	"testing"
-
-	r "github.com/anonx/sunplate/internal/reflect"
 )
 
 /*
@@ -292,86 +290,6 @@ func TestGet(t *testing.T) {
 	exp = ""
 	if r := get(vs, k, []int{10}); r != exp {
 		t.Errorf(errMsg, exp, r)
-	}
-}
-
-/*
-	Below are tests for code generation related functions and methods.
-*/
-
-func TestRender(t *testing.T) {
-	c := Context()
-	a := r.Arg{Name: "names", Type: &r.Type{Name: "[]string"}}
-	exp := `strconv.Strings(r.Form, "names")`
-	var expErr error
-	if r, err := c.Render("strconv", "r.Form", a); err != expErr || r != exp {
-		t.Errorf("Incorrect result of Render. Expected `%v`, `%v`;\ngot `%v`, `%v`.", exp, expErr, r, err)
-	}
-
-	a = r.Arg{Name: "names", Type: &r.Type{Name: "YoHoHo"}}
-	exp = ""
-	expErr = ErrUnsupportedType
-	if r, err := c.Render("strconv", "r.Form", a); err != expErr || r != exp {
-		t.Errorf("Incorrect result of Render. Expected `%v`, `%v`;\ngot `%v`, `%v`.", exp, expErr, r, err)
-	}
-}
-
-func TestContext(t *testing.T) {
-	c := Context()
-	supportedTypes := []string{
-		"bool", "string", "int", "int8", "int16", "int32", "int64",
-		"float32", "float64", "uint", "uint8", "uint16", "uint32", "uint64",
-
-		"[]bool", "[]string", "[]int", "[]int8", "[]int16", "[]int32", "[]int64",
-		"[]float32", "[]float64", "[]uint", "[]uint8", "[]uint16", "[]uint32", "[]uint64",
-	}
-	num := len(supportedTypes)
-	if l := len(c); l != num {
-		t.Errorf("Context returns incorrect number of arguments. Expected %d, got %d.", num, l)
-	}
-	for _, k := range supportedTypes {
-		if _, ok := c[k]; !ok {
-			t.Errorf(`Incorrect result of Context. Key "%s" is not found in %v.`, k, c)
-		}
-	}
-}
-
-func TestStrconvFunc(t *testing.T) {
-	if ok := !strconvFunc(r.Func{Name: "local"}); !ok {
-		t.Errorf(errMsg, ok, !ok)
-	}
-	if ok := !strconvFunc(r.Func{Name: "E"}); !ok {
-		t.Errorf(errMsg, ok, !ok)
-	}
-	ps := r.Args{
-		{
-			Name: "vs",
-			Type: &r.Type{
-				Name:    "Values",
-				Package: "url",
-			},
-		},
-		{
-			Name: "k",
-			Type: &r.Type{
-				Name: "string",
-			},
-		},
-		{
-			Name: "is",
-			Type: &r.Type{
-				Name: "...int",
-			},
-		},
-	}
-	if ok := !strconvFunc(r.Func{Name: "E", Params: ps}); !ok {
-		t.Errorf(errMsg, ok, !ok)
-	}
-	ps[0].Type = &r.Type{
-		Name: "SmthElse",
-	}
-	if ok := !strconvFunc(r.Func{Name: "E", Params: ps}); !ok {
-		t.Errorf(errMsg, ok, !ok)
 	}
 }
 
