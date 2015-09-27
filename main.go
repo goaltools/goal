608bbd3e5e335cdd6368b732dd0fd0c383259ad3
Alexander Kucherenko <alkchr@colegion.com>
2015-09-27 19:13:21 +0600

    main.go: reflect the latest changes in internal command subpackage

--- main.go
+++ main.go
@@ -5,69 +5,28 @@ package main
 
 import (
 	"os"
+	"strings"
 
-	"github.com/colegion/goal/commands/create"
-	"github.com/colegion/goal/commands/generate"
-	"github.com/colegion/goal/commands/run"
 	"github.com/colegion/goal/internal/command"
 	"github.com/colegion/goal/log"
 )
 
-// handlers is a map of registered subcommands (aka tools)
+// handlers is a stores information about the registered subcommands (aka tools)
 // the framework supports.
 var handlers = command.NewContext()
 
 func main() {
-	var trace bool
-
-	// Do not show stacktrace if something goes wrong
-	// in case tracing is turned off.
-	defer func() {
-		if !trace {
-			if err := recover(); err != nil {
-				// Do nothing, error message has already been printed
-				// and we do not need stack trace.
-			}
-		}
-	}()
-
-	// Enabling tracing if that is requested by a user.
-	command.Helpers["--trace"] = func(val string) {
-		if val == "true" {
-			trace = true
-		}
+	// Try to run the command user requested.
+	// Ignoring the first argument as it is name of the executable.
+	err := handlers.Run(os.Args[1:])
+	switch err {
+	case nil: // Handler's entry function has been successfully executed.
+		// Do nothing.
+	case command.ErrIncorrectArgs: // Incorrect command requested.
+		log.Warn.Printf(unknownCmd, strings.Join(os.Args, " "))
+	default: // Some other error has been received.
+		log.Error.Println(err)
 	}
-
-	// Try to run the subcommand user requested.
-	err := Handlers.Process(os.Args[1:]...)
-	if err == command.ErrIncorrectArgs { // The arguments were not correct.
-		log.Warn.Printf(unknownCmd, os.Args[1])
-		return
-	}
-	if err != nil { // The arguments were omitted.
-		Handlers.Process("help", "info") // Show a help message.
-		return
-	}
-}
-
-func init() {
-	// Register the supported subcommands.
-	Handlers.Register(create.Handler)
-	Handlers.Register(run.Handler)
-	Handlers.Register(generate.Handler)
-	Handlers.Register(helpHandler)
-
-	// Show header message when using new or help
-	// commands.
-	command.Helpers["new"] = showHeader
-	command.Helpers["run"] = showHeader
-	command.Helpers["help"] = showHeader
-}
-
-// showHeader prints a header message
-// with the name of the project.
-func showHeader(val string) {
-	log.Trace.Println(header)
 }
 
 var unknownCmd = `Unknown command "%s".
