de0a03e56c661635270b8a174ccc7af7312c18f0
Alexander Kucherenko <alkchr@colegion.com>
2015-10-18 21:29:30 +0600

    Extracted handlers init into a separate package for DRY

--- internal/skeleton/main.go
+++ internal/skeleton/main.go
@@ -6,27 +6,16 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/colegion/goal/internal/skeleton/assets/handlers"
-	"github.com/colegion/goal/internal/skeleton/routes"
+	"github.com/colegion/goal/internal/skeleton/utils"
 
-	"github.com/colegion/contrib/configs/iniflag"
 	"github.com/colegion/contrib/servers/grace"
 )
 
 var addr = flag.String("http.addr", ":8080", "address the application will listen on")
 
 func main() {
-	// Parse configuration files and flags.
-	err := iniflag.Parse("config/config.ini")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Initialization of handlers.
-	handlers.Init()
-
-	// Build the routes and handler.
-	handler, err := routes.List.Build()
+	// Initialize app's HTTP handler.
+	handler, err := utils.InitHandler("config/config.ini")
 	if err != nil {
 		log.Fatal(err)
 	}
