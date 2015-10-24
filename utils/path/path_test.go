75260a71a580c1484dcfb9684d48d4bcdd71145d
Alexander Kucherenko <alkchr@colegion.com>
2015-10-24 13:16:30 +0600

    Fixed processing of relative import paths

--- utils/path/path_test.go
+++ utils/path/path_test.go
@@ -62,6 +62,26 @@ func TestImportToAbsolute(t *testing.T) {
 			imp: "github.com/colegion/goal",
 			exp: filepath.Join(build.Default.GOPATH, "src", "github.com/colegion/goal"),
 		},
+		{
+			imp: "..",
+			exp: filepath.Join(value(os.Getwd()).(string), "../"),
+		},
+		{
+			imp: "../",
+			exp: filepath.Join(value(os.Getwd()).(string), "../"),
+		},
+		{
+			imp: "../../../../../../",
+			exE: true,
+		},
+		{
+			imp: ".",
+			exp: value(os.Getwd()).(string),
+		},
+		{
+			imp: "./",
+			exp: value(os.Getwd()).(string),
+		},
 	} {
 
 		if p, err := ImportToAbsolute(v.imp); p != v.exp || v.exE && err == nil || !v.exE && err != nil {
@@ -73,6 +93,42 @@ func TestImportToAbsolute(t *testing.T) {
 	}
 }
 
+func TestImportToAbsolute_GetwdError(t *testing.T) {
+	pushd(t)
+
+	p, err := ImportToAbsolute("../")
+	if p != "" || err == nil {
+		t.Errorf(`Getwd failed, error expected. Got "%s", "%v".`, p, err)
+	}
+
+	popd(t)
+}
+
 func value(v interface{}, err error) interface{} {
 	return v
+}
+
+func pushd(t *testing.T) {
+	// Preparing a state for os.Getwd to fail.
+	dir := "./someDirectoryThatDoesNotExistYet"
+	err := os.Mkdir(dir, 0755)
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.Chdir(dir)
+	if err != nil {
+		t.Error(err)
+	}
+	err = os.Remove(filepath.Join("../", dir))
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func popd(t *testing.T) {
+	// Repairing old state for running tests from current dir.
+	err := os.Chdir("../")
+	if err != nil {
+		t.Error(err)
+	}
 }
