46486788d8c5356603ac0a266b23a91344ae660e
alconaut <alconautgh@yandex.com>
2015-10-26 16:14:29 +0600

    Use server subpackage in main.go

--- internal/skeleton/main.go
+++ internal/skeleton/main.go
@@ -2,31 +2,11 @@
 package main
 
 import (
-	"flag"
 	"log"
-	"net/http"
 
-	"github.com/colegion/goal/internal/skeleton/utils"
-
-	"github.com/colegion/contrib/servers/grace"
+	"github.com/colegion/goal/internal/skeleton/server"
 )
 
-var addr = flag.String("http.addr", ":8080", "address the application will listen on")
-
 func main() {
-	// Initialize app's HTTP handler.
-	handler, err := utils.InitHandler("config/config.ini")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Prepare a new server.
-	s := &http.Server{
-		Addr:    *addr,
-		Handler: handler,
-	}
-
-	// Start the server.
-	log.Printf(`Listening on "%s".`, s.Addr)
-	log.Fatal(grace.Serve(s))
+	log.Fatal(server.Start("config/app.ini"))
 }
