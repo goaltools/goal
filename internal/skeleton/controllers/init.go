36c5d8f08b1aecfcd7fbebc299e506983a3f1b78
alconaut <alconautgh@yandex.com>
2015-12-28 15:29:19 +0600

    Use of 'goal run' instead of 'go generate'

--- internal/skeleton/controllers/init.go
+++ internal/skeleton/controllers/init.go
@@ -8,19 +8,6 @@ import (
 	"github.com/colegion/contrib/controllers/templates"
 )
 
-// The line below tells golang's generate command you want
-// it to scan your controllers and generate handler functions
-// from them using rules of goal toolkit.
-// Please, do not delete it unless you know what you are doing.
-//
-//go:generate goal generate handlers --input ./ --output ../assets/handlers
-
-// The line below tells golang's generate command you want
-// it to scan your views and generate a listing.
-// Please, do not delete it unless you know what you are doing.
-//
-//go:generate goal generate listing --input ../views --output ../assets/views
-
 // Controllers is a struct that should be embedded into every controller
 // of your app to make methods and fields provided by standard controllers available.
 type Controllers struct {
