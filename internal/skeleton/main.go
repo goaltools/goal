d733bd9f686797f504b9d40946d35f67d3214aaf
alconaut <alconautgh@yandex.com>
2016-02-14 23:15:33 +0600

    Remove routes and server packages moving necessary logics to main.go

--- internal/skeleton/main.go
+++ internal/skeleton/main.go
@@ -2,11 +2,45 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"net/http"
 
-	"github.com/colegion/goal/internal/skeleton/server"
+	"github.com/colegion/goal/internal/skeleton/assets/handlers"
+
+	"github.com/colegion/contrib/routers/denco"
+	"github.com/colegion/contrib/servers/grace"
+	"github.com/goaltools/iniflag"
 )
 
+var addr = flag.String("http.addr", ":8080", "HTTP address the app must listen on")
+
+// main parses configuration files making the values available
+// to flags of the app, builds a handler using routes and handlers
+// of the automatically generated package, and starts a new server.
 func main() {
-	log.Fatal(server.Start("config/app.ini"))
+	// Parse configuration files and flags.
+	err := iniflag.Parse("config/app.ini")
+	assertNil(err)
+
+	// Initialize and build routes.
+	h, err := denco.Build(handlers.Init())
+	assertNil(err)
+
+	// Allocate and run a new HTTP server.
+	s := &http.Server{
+		Addr:    *addr,
+		Handler: h,
+	}
+	err = grace.Serve(s)
+	assertNil(err)
+}
+
+// assertNil gets an error as an argument
+// and makes sure it is nil.
+// If not, it terminates the program.
+func assertNil(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
