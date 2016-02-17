d7b7e602af5e3bec46c6ae74f75900e491b12e89
alconaut <alconautgh@yandex.com>
2016-02-17 16:33:14 +0600

    Splitted packages.go into multiple files

--- tools/generate/handlers/packages.go
+++ tools/generate/handlers/packages.go
@@ -1,11 +1,6 @@
 package handlers
 
 import (
-	"fmt"
-	"go/ast"
-	r "reflect"
-	"strings"
-
 	a "github.com/colegion/goal/internal/action"
 	"github.com/colegion/goal/internal/log"
 	"github.com/colegion/goal/internal/reflect"
@@ -18,80 +13,6 @@ import (
 //		- Controllers
 type packages map[string]controllers
 
-// controllers stores information about application controllers
-// in the following form:
-//	- Name of the controller:
-//		- Controller representation itself
-// and their Init functions.
-type controllers struct {
-	data map[string]controller
-	init *reflect.Func
-}
-
-// parents represents a set of parent controllers.
-type parents []parent
-
-// parent represents embedded struct that should be scanned for
-// actions and magic methods.
-type parent struct {
-	ID     int    // Unique number that is used for generation of import names.
-	Import string // Import path of the structure, e.g. "github.com/colegion/goal/template" or "".
-	Name   string // Name of the structure, e.g. "Template".
-}
-
-// field represents a field of a structure that must be automatically binded.
-type field struct {
-	Name string // Name of the field, e.g. "Request".
-	Type string // Type of the binding, e.g. "request" or "action".
-}
-
-// controller is a type that represents application controller,
-// a structure that has actions.
-type controller struct {
-	Actions reflect.Funcs // Actions are methods that implement action.Result interface.
-	After   *reflect.Func // Magic method that is executed after actions if they return nil.
-	Before  *reflect.Func // Magic method that is executed before every action.
-
-	Comments reflect.Comments // A group of comments right above the controller declaration.
-	File     string           // Name of the file where this controller is located.
-	Parents  parents          // A list of embedded structs that should be parsed.
-
-	Fields []field          // A list of fields that require binding.
-	Routes [][]routes.Route // Routes concatenated with prefixes. len(Routes) = len(Actions)
-}
-
-// Package returns a unique package name that may be used in templates
-// concatenated with some arbitarry suffix strings.
-// If parent is from the local package, empty string will be returned.
-// This method is useful to generate in templates things like:
-//	uniquePkgName "github.com/user/project"
-// and:
-//	uniquePkgName.Application.Index() // Package name and dot suffix.
-func (p parent) Package(suffixes ...string) string {
-	if p.Import == "" {
-		return ""
-	}
-	s := fmt.Sprintf("c%d", p.ID)
-	for i := range suffixes {
-		s += suffixes[i]
-	}
-	return s
-}
-
-// IgnoredArgs gets an action Func as input parameter
-// and returns blank identifiers for parameters
-// other than the first one.
-// E.g. if the action returns action.Result, error, bool,
-// this method will return ", _, _".
-// So it can be used during code generation.
-func (c controller) IgnoredArgs(f *reflect.Func) (s string) {
-	n := len(f.Results) - 1 // Ignore action.Result.
-	if n > 0 {
-		s = strings.Repeat(", _", n)
-	}
-	return
-}
-
 // processPackage gets an import path of a package and its
 // route prefixes, processes this data, and
 // extracts controllers + actions.
@@ -102,84 +23,30 @@ func (ps packages) processPackage(importPath string, prefs routes.Prefixes) {
 		log.Error.Panic(err)
 	}
 	p := reflect.ParseDir(dir, false)
-	cs := ps.extractControllers(p, prefs)
-	if len(cs.data) > 0 {
+	cs := ps.extractControllers(importPath, p, prefs)
+	if len(cs.list) > 0 {
 		ps[importPath] = controllers{
-			data: cs.data,
+			list: cs.list,
 			init: ps.extractInitFunc(p),
 		}
 	}
 }
 
-// needBindingField gets a package, an index of struct and index of field
-// in the struct. The field is checked whether it has a reserved tag
-// and it is of correct type.
-func (ps packages) needBindingField(pkg *reflect.Package, i, j int) *field {
-	f := &field{}
-	t := pkg.Structs[i].Fields[j]
-	switch st := r.StructTag(t.Tag).Get("bind"); st {
-	case "response":
-		// Make sure "http" package is imported.
-		n, ok := pkg.Imports.Name(pkg.Structs[i].File, "net/http")
-		if !ok || t.Type.String() != fmt.Sprintf("%s.ResponseWriter", n) {
-			log.Warn.Printf(
-				`Field "%s" in controller "%s" cannot be binded. Response must be of type "(net/http).ResponseWriter".`,
-				t.Name, pkg.Structs[i].Name,
-			)
-			return nil
-		}
-		f.Type = st
-	case "request":
-		// Make sure "http" package is imported.
-		n, ok := pkg.Imports.Name(pkg.Structs[i].File, "net/http")
-		if !ok || t.Type.String() != fmt.Sprintf("*%s.Request", n) {
-			log.Warn.Printf(
-				`Field "%s" in controller "%s" cannot be binded. Request must be of type "*(net/http).Request".`,
-				t.Name, pkg.Structs[i].Name,
-			)
-			return nil
-		}
-		f.Type = st
-	case "controller":
-		if t.Type.String() != "string" {
-			log.Warn.Printf(
-				`Field "%s" in controller "%s" cannot be binded. Controller name must be of type "string".`,
-				t.Name, pkg.Structs[i].Name,
-			)
-			return nil
-		}
-		f.Type = "controller"
-	case "action":
-		if t.Type.String() != "string" {
-			log.Warn.Printf(
-				`Field "%s" in controller "%s" cannot be binded. Action name must be of type "string".`,
-				t.Name, pkg.Structs[i].Name,
-			)
-			return nil
-		}
-		f.Type = st
-	default:
-		return nil
-	}
-	f.Name = t.Name
-	if !ast.IsExported(f.Name) {
-		log.Warn.Printf(
-			`Field "%s" in controller "%s" must be public in order to be binded.`,
-			f.Name, pkg.Structs[i].Name,
-		)
-		return nil
-	}
-	log.Trace.Printf(`Field %s will be binded to "%s".`, f.Name, f.Type)
-	return f
-}
-
-// scanFields expects a package and an index of structure in that package.
+// scanFields gets an import path, a package itself,
+// and an index of structure in that package.
 // It scans the structure looking for two kinds of fields:
 // anonymously embedded types and named fields with special tags.
 // Every anonymously embedded type is checked recursively regarding being a controller.
 // As a result a list of all found fields with the tags and
 // types in a form of []parent are returned.
-func (ps packages) scanFields(pkg *reflect.Package, i int) (fs []field, prs []parent) {
+func (ps packages) scanFields(impPath string, pkg *reflect.Package, i int) ([]field, parents) {
+	// Allocate result []field and parents types.
+	fs := []field{}
+	prs := parents{
+		childImport: impPath,
+		list:        []parent{},
+	}
+
 	// Iterating over fields of the structure.
 	for j := range pkg.Structs[i].Fields {
 		// Check whether the field requires binding.
@@ -201,60 +68,33 @@ func (ps packages) scanFields(pkg *reflect.Package, i int) (fs []field, prs []pa
 
 		// Add the field to the list of results.
 		imp, _ := pkg.Imports.Value(pkg.Structs[i].File, pkg.Structs[i].Fields[j].Type.Package)
-		p, _ := path.CleanImport(imp)
-		prs = append(prs, parent{
-			Import: p,
+		if imp == "" { // If the import is empty, the embedded structure is from the same package.
+			imp = impPath
+		}
+		prs.list = append(prs.list, parent{
+			Import: imp,
 			Name:   pkg.Structs[i].Fields[j].Type.Name,
 		})
 
 		// Check whether this import has already been processed.
 		// If not, do it now.
-		if _, ok := ps[imp]; imp != "" && !ok {
-			ps.processPackage(p, routes.ParseTag(pkg.Structs[i].Fields[j].Tag))
+		if _, ok := ps[imp]; !ok {
+			ps.processPackage(imp, routes.ParseTag(pkg.Structs[i].Fields[j].Tag))
 		}
 	}
-	return
+	return fs, prs
 }
 
-func (ps packages) extractInitFunc(pkg *reflect.Package) *reflect.Func {
-	res, _ := pkg.Funcs.FilterGroups(func(f *reflect.Func) bool {
-		if f.Name != "Init" {
-			return false
-		}
-		if f.Recv != nil {
-			return false
-		}
-		if len(f.Params) != 1 {
-			return false
-		}
-		if f.Params[0].Type.Name != "Values" {
-			return false
-		}
-		v, _ := pkg.Imports.Value(f.File, f.Params[0].Type.Package)
-		if v != "net/url" {
-			return false
-		}
-		log.Trace.Printf(`Magic "%s" function will be added to generated "%s" file.`, f.Name, f.File)
-		return true
-	}, func(f *reflect.Func) bool {
-		return true
-	})
-	if len(res[0]) > 0 {
-		return &res[0][0]
-	}
-	return nil
-}
-
-// extractControllers gets a reflect.Package type and returns
+// extractControllers gets an import path, a parsed reflect.Package itself, and returns
 // a slice of controllers that are found there.
-func (ps packages) extractControllers(pkg *reflect.Package, prefs routes.Prefixes) controllers {
+func (ps packages) extractControllers(impPath string, pkg *reflect.Package, prefs routes.Prefixes) controllers {
 	// Initialize function that will be used for detection of actions.
 	action := a.Func(pkg)
 
 	// Iterating through all available structures and checking
 	// whether those structures are controllers (i.e. whether they have actions).
 	cs := controllers{
-		data: map[string]controller{},
+		list: []controller{},
 	}
 	for i := 0; i < len(pkg.Structs); i++ {
 		// Make sure the structure has methods.
@@ -290,10 +130,12 @@ func (ps packages) extractControllers(pkg *reflect.Package, prefs routes.Prefixe
 		}
 
 		// Parse parent controllers and fields that require binding.
-		fs, prs := ps.scanFields(pkg, i)
+		fs, prs := ps.scanFields(impPath, pkg, i)
 
 		// Add a new controller to the list of results.
-		cs.data[pkg.Structs[i].Name] = controller{
+		cs.list = append(cs.list, controller{
+			Name: pkg.Structs[i].Name,
+
 			Actions: as[0],
 			After:   firstFunc(as[1]),
 			Before:  firstFunc(as[2]),
@@ -304,9 +146,54 @@ func (ps packages) extractControllers(pkg *reflect.Package, prefs routes.Prefixe
 
 			Fields: fs,
 			Routes: rs,
-		}
+		})
 	}
 	return cs
+}
+
+// extractInitFunction returns an "Init" function found in a controller package,
+// if it does exist.
+func (ps packages) extractInitFunc(pkg *reflect.Package) *reflect.Func {
+	// Iterate over all available functions.
+	res, _ := pkg.Funcs.FilterGroups(func(f *reflect.Func) bool {
+		// Find the one with name "Init".
+		if f.Name != "Init" {
+			return false
+		}
+
+		// Make sure it is a function rather than a method.
+		if f.Recv != nil {
+			return false // This must never happen, pkg.Funcs does not contain methods.
+		}
+
+		// Make sure the function takes one argument.
+		if len(f.Params) != 1 {
+			return false
+		}
+
+		// The argument must be of types Values.
+		if f.Params[0].Type.Name != "Values" {
+			return false
+		}
+
+		// The Values type must be from package "net/url".
+		v, _ := pkg.Imports.Value(f.File, f.Params[0].Type.Package)
+		if v != "net/url" {
+			return false
+		}
+
+		// If we've achieved this point, "Init" function is found.
+		log.Trace.Printf(`Magic "%s" function will be added to generated "%s" file.`, f.Name, f.File)
+		return true
+	}, func(f *reflect.Func) bool {
+		// Return every of the found "Init" functions.
+		return true
+	})
+	// If result is not empty, return the first function.
+	if len(res[0]) > 0 {
+		return firstFunc(res[0])
+	}
+	return nil
 }
 
 // firstFunc gets a list of functions and returns the first element of it.
---
+++ tools/generate/handlers/parents.go
@@ -0,0 +1,66 @@
+package handlers
+
+import (
+	"fmt"
+)
+
+// parents represents a set of relative controllers.
+type parents struct {
+	childImport string
+	list        []parent
+}
+
+// parent represents embedded struct that should be scanned for
+// actions and magic methods and/or the opposite of that (i.e.
+// all controllers that import the current one).
+type parent struct {
+	ID     int    // Unique number that is used for generation of import names.
+	Import string // Import path of the structure, e.g. "github.com/colegion/goal/template".
+	Name   string // Name of the structure, e.g. "Template".
+}
+
+// Package returns a unique package name that may be used in templates
+// concatenated with some arbitarry suffix strings.
+// If a parent is from the same package, empty string will be returned.
+// This method is useful to generate things like:
+//	import (
+//		// {{ .parent.Package .import }} "{{ .parent.Import }}"
+//		uniquePkgName "github.com/user/project"
+//	)
+// and (accessor & dot suffix):
+//	// {{ .parent.Package "." }}Application.Index()
+//	uniquePkgName.Application.Index()
+func (p parent) Package(impPath string, suffixes ...string) string {
+	// If the parent is from the same package as the child,
+	// we don't need an accessor.
+	if p.Import == impPath {
+		return ""
+	}
+	// Otherwise, use package ID and some suffix.
+	s := fmt.Sprintf("c%d", p.ID)
+	for i := range suffixes {
+		s += suffixes[i]
+	}
+	return s
+}
+
+// All returns all parent controllers of a controller including
+// grandparents, grandgrandparents, and so forth.
+// The result is in the order the controllers must be initialized
+// and their special actions must be called.
+// I.e. grandparents first, then parents, then children.
+func (ps parents) All(pkgs packages) (cs controllers) {
+	for i := range ps.list {
+		// Make sure current parent is a controller rather than
+		// some embedded struct.
+		pkg, ok := pkgs[ps.list[i].Import] // Make sure it's from the package with controllers.
+		if !ok {
+			continue // Controllers were not detected in the package of this parent, skip it.
+		}
+		c := pkg.Controller(ps.list[i].Name) // Make sure it is a controller, not some struct.
+		if c == nil {
+			continue // This parent is from a package with controllers but not a controller, skip it.
+		}
+	}
+	return
+}
